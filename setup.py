bbf6d12448ae3f29c1af5ed1d1940d0066f5129e
Egil <egil.moller@freecode.no>
2021-12-08 09:37:23 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 
 setuptools.setup(
     name='libgeosuitesnd',
-    version='0.0.4',
+    version='0.0.5',
     description='Parser for the GeoSuite<tm> SND export format',
     long_description="""Parser for the GeoSuite<tm> SND export format""",
     long_description_content_type="text/markdown",
